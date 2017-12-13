c5e5c076a6825157f4180413ecd4606cd9e55ee7
David Mendelovits <david.mendelovits@gmail.com>
2017-12-13 15:07:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function whisper(string) {
 return string.toLowerCase()}
 
 function logShout(string) {
-if logshout(string=string.touppercase) {  
+if logshout(string=string.touppercase) {
 console.log(string)
 }
