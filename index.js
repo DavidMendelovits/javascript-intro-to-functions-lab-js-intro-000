21d79388221cb3773187f2189d433441211cb7c8
David Mendelovits <david.mendelovits@gmail.com>
2017-12-13 15:43:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ console.log(string.toUpperCase()) }
 function logWhisper(string) {
 console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+if(string.toLowerCase===string) {
+return("I can't hear you!")  
+}
+}
