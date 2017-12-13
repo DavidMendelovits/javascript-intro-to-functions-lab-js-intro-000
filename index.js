d47ad4da97ad782c584676bc4aa203ddbaa7ddf6
David Mendelovits <david.mendelovits@gmail.com>
2017-12-13 16:48:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,7 @@ if(string.toLowerCase()===string) {
 return ('I can\'t hear you!')}
 if(string.toUpperCase()===string) {
 return ('YES INDEED!')}
+if('I love you, Grandma.'===string) {
+return ('I love you, too.')  
+}
 }
